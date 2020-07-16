c824f44b652351fd9f91786e8bb7afa01c51b07d
xiaodongli1986 <xiaodongli@mail.kias.re.kr>
2020-07-16 12:27:48 +0800

    minor changes

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,10 @@
 git pull
-git add *.sh
+git add -f *.sh
 git add README.md
-git add src/*.f90
+git add -f src/*.f90
 git add src/UsefulCodes/*.f90
-git add src/*.py
-git add src/*.sh
+git add -f src/*.py
+git add -f src/*.sh
 git add src/Makefile
 git add bin/LSS_sayhellotoraphael
 git add data/DEC_vs_RAspan-to-DECspan.txt
